12d01d66f8ec358a2bf62db577a679bc8c8fe6db
Carolyn Atterbury <carolynatterbury@gmail.com>
2017-02-17 12:55:00 -0700

    vid feed color tracking outputting to sound

--- color-tracker.js
+++ color-tracker.js
@@ -15,11 +15,17 @@ function initVideoTracker() {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
     var tracker = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
-
+    // a 4 voice Synth
+    var polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster();
+    
     tracking.track('#video', tracker, { camera: true });
     tracker.on('track', function(event) {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	event.data.forEach(function(rect) {
+
+	    //play a chord
+	    polySynth.triggerAttackRelease(["C4", "E4", "G4", "B4"], "2n");
+
 	    context.strokeStyle = rect.color;
 	    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
         });
