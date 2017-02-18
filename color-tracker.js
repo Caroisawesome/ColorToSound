c066db88242b6e8e513519966f92559fbd8a16c2
Carolyn Atterbury <carolynatterbury@gmail.com>
2017-02-18 13:31:45 -0700

    distortion effect added to y axis

--- color-tracker.js
+++ color-tracker.js
@@ -8,7 +8,8 @@ class ColorNoiseMaker {
 	this.context = canvas.getContext('2d');
 	this.colorTracker = new tracking.ColorTracker(this.colors);
 	this.polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster();
-
+	this.distortion = new Tone.Distortion().toMaster();
+	
 	this.CScale = ["C3","D3","E3","F3","G3","A3","B3","C4","D4","E4","F4","G4","A4","B4"];
 	
 	this.initTracking();
@@ -23,8 +24,14 @@ class ColorNoiseMaker {
 	    event.data.forEach(function(rect) {
 
 		if (self.makeNoise) {
+
+		    // calcluate distortion levels and add distortion to output
+		    const distortionLevel = parseFloat(rect.y)/parseFloat(450);
+		    self.distortion.wet.value = 1.0 - distortionLevel;
+		    self.polySynth.connect(self.distortion);
 		    const soundIndex = parseInt(rect.x) % 40;
-		    //play a chord
+
+		    // play
 		    self.polySynth.triggerAttackRelease([self.CScale[soundIndex], self.CScale[soundIndex+2]], "2n");
 		}
 		
